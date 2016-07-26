cd4262493561edaf25a73a71d9f8fe715baabe54
Alberto Núñez <nuakosta@gmail.com>
2016-07-26 09:27:31 +0200

    Update steelseries-engine to 3.8.2
    Updates steelseries-engine to 3.8.2. Changes the download URL, since the current one doesn't work anymore.

--- Casks/steelseries-engine.rb
+++ Casks/steelseries-engine.rb
@@ -1,9 +1,8 @@
 cask 'steelseries-engine' do
-  version '3.7.1'
-  sha256 '0f50666e3a97c81f2c3b3825d1d7d8fa7ec0d458b12aee4968dd5442af648c0c'
+  version '3.8.2'
+  sha256 'e578e1e4ebfe6e30214a5b24d1191351f0666b7499249b0117ac948401cab008'
 
-  # steelseriescdn.com was verified as official when first introduced to the cask
-  url "http://downloads.steelseriescdn.com/drivers/engine/SteelSeriesEngine#{version}.pkg"
+  url "https://dysluvtcgugeg.cloudfront.net/drivers/engine/SteelSeriesEngine#{version}.pkg"
   name 'SteelSeries Engine 3'
   homepage 'https://steelseries.com/engine'
   license :gratis
